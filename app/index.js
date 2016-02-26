09893e7af6a4ac90791677c9ad9d64990b212e56
Chai Phonbopit <Phonbopit@gmail.com>
2016-02-26 16:39:12 +0700

    Update index.js on fly

--- app/index.js
+++ app/index.js
@@ -5,6 +5,12 @@ app.get('/', function(req, res) {
   res.json({message: 'Hello World'});
 });
 
+app.get('/hello', function(req, res) {
+  res.json({
+    message: 'Hello'
+  });
+});
+
 app.listen(7777, function() {
   console.log('Application is running on port 7777');
 });
